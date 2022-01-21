5de96d4b900b37da24e5dfc7677687fbb1d7aa48
RGomes97 <rubensgagostinho@outlook.com>
2022-01-21 19:52:44 -0300

    iterating the list without loop

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = string.Join(Environment.NewLine, QuestionClass.NamesList);
+            Console.Write(names);
+        }
+    }
+}
---
+++ ConsoleApp1/QuestionClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class QuestionClass
+    {
+        public static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+    }
+}
