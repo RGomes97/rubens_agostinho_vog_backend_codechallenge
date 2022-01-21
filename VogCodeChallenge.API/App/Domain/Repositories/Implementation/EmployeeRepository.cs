a54ffb2f9c03adb3ee72934b8a2d8220cfaa56ca
RGomes97 <rubensgagostinho@outlook.com>
2022-01-21 19:46:55 -0300

    showing how to connect a database and manipulate the data

--- VogCodeChallenge.API/App/Domain/Repositories/Implementation/EmployeeRepository.cs
+++ VogCodeChallenge.API/App/Domain/Repositories/Implementation/EmployeeRepository.cs
@@ -8,18 +8,33 @@ namespace VogCodeChallenge.API.App.Domain.Repositories.Implementation
 {
     public class EmployeeRepository : IEmployeeRepository
     {
+        //to implement a database i would like to use a dbContext from entityframework to access the database and manipulate the data like this:
+        //public EmployeeRepository(VogCodeChallengeContext context)
+        //{
+        //    _dbContext = context;
+        //}
+        //To do it like this I just would have to Map my database Models and use like this
+        //public async Task<IEnumerable<Employee>> GetAllAsync()
+        //{
+        //    return await context.Employees.ToListAsync();
+        //}
+        //public async Task<IList<Employee>> ListAllAsync()
+        //{
+        //    return await context.Employees.ToListAsync();
+        //}
+
         public async Task<IEnumerable<Employee>> GetAllAsync()
         {
-            List<Department> departmentList = EntityLoader.GetDefaultDepartments();
-            List<Employee> employees = EntityLoader.GetDefaultEmployees(departmentList);
+            var departmentList = EntityLoader.GetDefaultDepartments();
+            var employees = EntityLoader.GetDefaultEmployees(departmentList);
 
             return await Task.FromResult(employees);
         }
 
         public async Task<IList<Employee>> ListAllAsync()
         {
-            List<Department> departmentList = EntityLoader.GetDefaultDepartments();
-            List<Employee> employees = EntityLoader.GetDefaultEmployees(departmentList);
+            var departmentList = EntityLoader.GetDefaultDepartments();
+            var employees = EntityLoader.GetDefaultEmployees(departmentList);
 
             return await Task.FromResult(employees);
         }
