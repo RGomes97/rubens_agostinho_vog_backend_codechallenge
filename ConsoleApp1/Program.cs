df42be0b184e8d06b9c0cff815b4bd2a72281b11
RGomes97 <rubensgagostinho@outlook.com>
2022-01-21 20:28:32 -0300

    method TestModule

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,40 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string names = string.Join(Environment.NewLine, QuestionClass.NamesList);
-            Console.Write(names);
+            Console.WriteLine(names);
+            Console.WriteLine($"the result is {Convert.ToString(TESTModule(1))}");
+        }
+
+        private static dynamic TESTModule(dynamic value)
+        {
+            switch (value.GetType().Name)
+            {
+                case ("Int32"):
+                    var intResult = Convert.ToInt32(value);
+                    if (intResult >= 1 && intResult <= 4)
+                        return intResult * 2;
+                    if (intResult > 4)
+                        return intResult * 3;
+                    if (intResult < 1)
+                        throw new InvalidOperationException("Invalid value");
+                    break;
+                case ("Double"):
+                    var doubleResult = Convert.ToDouble(value);
+                    if (doubleResult >= 1.0 && doubleResult <= 2.0)
+                        return 3.0;
+                    break;
+                case ("String"):
+                    return value.ToString().ToUpperInvariant();
+                default:
+                    return value;
+            }
+
+            return 0;
+        }
+
+        public bool IsValueType<T>()
+        {
+            return typeof(T).IsValueType;
         }
     }
 }
